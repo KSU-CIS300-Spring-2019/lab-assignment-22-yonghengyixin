3bb1c693075688dd0fc168f5b039d865ea4aedd8
Rod Howell <rhowell@ksu.edu>
2019-03-18 11:26:11 -0500

    Lab 21 solution.

--- Ksu.Cis300.TrieLibrary.Tests/TrieTests.cs
+++ Ksu.Cis300.TrieLibrary.Tests/TrieTests.cs
@@ -1,1 +1,214 @@
-﻿// Add unit tests here.
+﻿/* TrieTests.cs
+ * Author: Rod Howell
+ */
+
+namespace Ksu.Cis300.TrieLibrary.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    /// <summary>
+    /// Unit tests for the Trie class.
+    /// </summary>
+    [TestFixture]
+    public class TrieTests
+    {
+        /// <summary>
+        /// Tests looking up an empty string in an empty trie.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestAEmptyContainsEmptyString()
+        {
+            Trie t = new Trie();
+            Assert.That(t.Contains(""), Is.False);
+        }
+
+        /// <summary>
+        /// Tests looking up a nonempty string in an empty trie.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestAEmptyContainsNonemptyString()
+        {
+            Trie t = new Trie();
+            Assert.That(t.Contains("word"), Is.False);
+        }
+
+        /// <summary>
+        /// Tests looking up a string with an invalid character.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestAEmptyContainsInvalidString()
+        {
+            Trie t = new Trie();
+            Assert.That(t.Contains("`"), Is.False);
+        }
+
+        /// <summary>
+        /// Tests lookup a longer string with an invalid character.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestAEmptyContainsLongerInvalidString()
+        {
+            Trie t = new Trie();
+            Assert.That(t.Contains("ab{d"), Is.False);
+        }
+
+        /// <summary>
+        /// Adds the empty string to an empty trie and looks it up.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestBAddEmptyLookItUp()
+        {
+            Trie t = new Trie();
+            t.Add("");
+            Assert.That(t.Contains(""), Is.True);
+        }
+
+        /// <summary>
+        /// Tests adding a string with an invalid character.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestBAddInvalid()
+        {
+            Exception e = null;
+            Trie t = new Trie();
+            try
+            {
+                t.Add("`");
+            }
+            catch (Exception ex)
+            {
+                e = ex;
+            }
+            Assert.That(e, Is.Not.Null.And.TypeOf(typeof(ArgumentException)));
+        }
+
+        /// <summary>
+        /// Tests adding another invalid string.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestBAddInvalid2()
+        {
+            Exception e = null;
+            Trie t = new Trie();
+            try
+            {
+                t.Add("{");
+            }
+            catch (Exception ex)
+            {
+                e = ex;
+            }
+            Assert.That(e, Is.Not.Null.And.TypeOf(typeof(ArgumentException)));
+        }
+
+        /// <summary>
+        /// Adds a 1-letter word and looks it up.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestCAddShortWordLookItUp()
+        {
+            Trie t = new Trie();
+            t.Add("i");
+            Assert.That(t.Contains("i"), Is.True);
+        }
+
+        /// <summary>
+        /// Adds a longer word and looks it up.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDAddLongWordLookItUp()
+        {
+            Trie t = new Trie();
+            t.Add("word");
+            Assert.That(t.Contains("word"), Is.True);
+        }
+
+        /// <summary>
+        /// Adds a word and looks up a prefix that should not be in the trie.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDAddWordLookUpPrefix()
+        {
+            Trie t = new Trie();
+            t.Add("word");
+            Assert.That(t.Contains("wo"), Is.False);
+        }
+
+        /// <summary>
+        /// Strings to add to the trie for the following tests.
+        /// </summary>
+        private string[] _words = new string[]
+        {
+            "cab",
+            "dog",
+            "cart",
+            "car",
+            "cable"
+        };
+
+        /// <summary>
+        /// Builds a trie containing the strings in _words.
+        /// </summary>
+        /// <returns>The resulting trie.</returns>
+        private Trie LoadTrie()
+        {
+            Trie t = new Trie();
+            foreach (string s in _words)
+            {
+                t.Add(s);
+            }
+            return t;
+        }
+
+        /// <summary>
+        /// Loads a trie with five words and looks one up.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestELookupInLarger()
+        {
+            Trie t = LoadTrie();
+            Assert.That(t.Contains("dog"), Is.True);
+        }
+
+        /// <summary>
+        /// Looks up a prefix that was added after the longer word.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestELookupPrefixAddedAfter()
+        {
+            Trie t = LoadTrie();
+            Assert.That(t.Contains("car"), Is.True);
+        }
+
+        /// <summary>
+        /// Looks up a prefix that was added before the longer word.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestELookupPrefixAddedBefore()
+        {
+            Trie t = LoadTrie();
+            Assert.That(t.Contains("cab"), Is.True);
+        }
+
+        /// <summary>
+        /// Looks up a word that has a prefix added earlier.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestELookupWordWithPrefixAddedBefore()
+        {
+            Trie t = LoadTrie();
+            Assert.That(t.Contains("cable"), Is.True);
+        }
+
+        /// <summary>
+        /// Looks up a word that has a prefix added later.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestELookupWordWithPrefixAddedAfter()
+        {
+            Trie t = LoadTrie();
+            Assert.That(t.Contains("cart"));
+        }
+    }
+}
--- Ksu.Cis300.TrieLibrary/Trie.cs
+++ Ksu.Cis300.TrieLibrary/Trie.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* Trie.cs
+ * Author: Rod Howell
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,69 @@ using System.Threading.Tasks;
 
 namespace Ksu.Cis300.TrieLibrary
 {
+    /// <summary>
+    /// A node of a trie for storing strings made up of lower-case English letters.
+    /// </summary>
     public class Trie
     {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString;
+
+        /// <summary>
+        /// The children of this node.
+        /// </summary>
+        private Trie[] _children = new Trie[26];
+
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s">The string to look up.</param>
+        /// <returns>Whether the trie rooted at this node contains s.</returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else
+            {
+                int loc = s[0] - 'a';
+                if (loc < 0 || loc >= _children.Length || _children[loc] == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return _children[loc].Contains(s.Substring(1));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s">The string to add.</param>
+        public void Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                int loc = s[0] - 'a';
+                if (_children[loc] == null)
+                {
+                    _children[loc] = new Trie();
+                }
+                _children[loc].Add(s.Substring(1));
+            }
+        }
     }
 }
